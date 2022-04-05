4f50cced60f447ece29ab31dac636a7774b7b542
Sanae G <sanae.gassa@gmail.com>
2022-04-05 16:16:59 +0200

    first Version Design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Header from "./components/Header";
-import Card from "./components/Card";
-import Navbar from "./components/Navbar";
+import Header from './components/Header';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <Header />
-    <Card />
-    <Navbar />
+      <Header />
+      <Card />
+      <Navbar />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,54 @@
+import styled from 'styled-components';
+
 function Card() {
-    return (
-<section>
-    <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="Morty Smith"></img>
-    <h2>Morty Smith</h2>
-    <button>Show more</button>
-</section>
-    );
-  }
-  
-  export default Card;
+  return (
+    <CardList>
+   <CardContent>
+      <img
+        src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        alt="Morty Smith"
+      ></img>
+      <CharacterName>Morty Smith</CharacterName>
+      <button>Show more</button>
+    </CardContent>
+    <CardContent>
+      <img
+        src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        alt="Morty Smith"
+      ></img>
+      <CharacterName>Morty Smith</CharacterName>
+      <button>Show more</button>
+    </CardContent>
+    <CardContent>
+      <img
+        src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+        alt="Morty Smith"
+      ></img>
+      <CharacterName>Morty Smith</CharacterName>
+      <button>Show more</button>
+    </CardContent>
+    </CardList>
+  );
+}
+
+export default Card;
+
+const CardContent = styled.div`
+    border: 1px solid black;
+    border-radius: 0.5em;
+    height: 10%;
+    display: flex;
+    flex-direction: column;
+`;
+
+const CardList = styled.section`
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 3em;
+`;
+
+const CharacterName = styled.h2`
+    text-align: center;
+`;
