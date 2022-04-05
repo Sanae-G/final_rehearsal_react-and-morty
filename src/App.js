e81a8fafa557cae093e453182bccc8d48f428aa3
Sanae G <sanae.gassa@gmail.com>
2022-04-05 14:12:38 +0200

    add Card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from "./components/Header";
+import Card from "./components/Card";
 
 function App() {
   return (
+    <main>
     <Header />
+    <Card />
+    </main>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+function Card() {
+    return (
+<section>
+    <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"></img>
+    <h2>Morty Smith</h2>
+</section>
+    );
+  }
+  
+  export default Card;
